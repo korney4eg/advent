a80b7f94d6f60dd42e9bdcaf38dc8dcbdc5a1dbc
dim <web.best.master@gmail.com>
2022-12-03 15:03:23 +0100

    advent 2022-03-2

--- 2022/3/main.go
+++ 2022/3/main.go
@@ -29,33 +29,47 @@ func main() {
 
 	sum := 0
 
-	for _, line := range linesList {
-		leftPart := line[0 : len(line)/2]
-		rightPart := line[len(line)/2 : len(line)]
-		commonLetter, err := getCommonLetter(leftPart, rightPart)
-		if err != nil {
-			log.Fatal(err)
-		}
-		//log.Println(commonLetter, getPoints(commonLetter))
-		sum += getPoints(commonLetter)
-
-		//log.Printf("\nline %s,\n leftPart %s,\nrightPart %s,\n", line, leftPart, rightPart)
-	}
-	log.Println(sum)
-}
-
-func getCommonLetter(first, second string) (string, error) {
-	for _, firstItem := range first {
-		for _, secondItem := range second {
-
-			if firstItem == secondItem {
-				return string(firstItem), nil
+	/*	for _, line := range linesList {
+			leftPart := line[0 : len(line)/2]
+			rightPart := line[len(line)/2 : len(line)]
+			commonLetter, err := getCommonLetter(leftPart, rightPart)
+			if err != nil {
+				log.Fatal(err)
 			}
+			//log.Println(commonLetter, getPoints(commonLetter))
+			sum += getPoints(commonLetter)
 
 			//log.Printf("\nline %s,\n leftPart %s,\nrightPart %s,\n", line, leftPart, rightPart)
 		}
+		log.Println(sum)
+	*/
+	for i := 0; i < len(linesList)/3; i++ {
+		firstPart := linesList[i*3]
+		secondPart := linesList[i*3+1]
+		thirdPart := linesList[i*3+2]
 
-		//log.Printf("\nline %s,\n leftPart %s,\nrightPart %s,\n", line, leftPart, rightPart)
+		commonLetter, err := getCommonLetter(firstPart, secondPart, thirdPart)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		sum += getPoints(commonLetter)
+
+	}
+
+	log.Println(sum)
+}
+
+func getCommonLetter(first, second, third string) (string, error) {
+	for _, firstItem := range first {
+		for _, secondItem := range second {
+			for _, thirdItem := range third {
+				if firstItem == secondItem && secondItem == thirdItem {
+					return string(firstItem), nil
+				}
+			}
+		}
 	}
 
 	return "", fmt.Errorf("Can not find item")
