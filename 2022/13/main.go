040391b1905d5601cae4b4f251d88670b2116c82
Aliaksei Karneyeu <aliaksei.karneyeu@openx.com>
2022-12-14 04:54:40 +0100

    13-2

--- 2022/13/main.go
+++ 2022/13/main.go
@@ -4,22 +4,40 @@ import (
 	"advent/2022/13/signals"
 	"advent/utils"
 	"log"
+	"sort"
 )
 
+type packet struct {
+	text string
+}
+
+type byText []packet
+
+func (a byText) Len() int           { return len(a) }
+func (a byText) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a byText) Less(i, j int) bool { return signals.IsRightOrder(a[i].text, a[j].text) }
+
 func main() {
-	index := 0
-	pair := 0
 	lines := utils.ReadFileToStringsList("2022/13/input.txt")
-	for i := 0; i < len(lines); i += 3 {
-		pair++
-		element1 := lines[i]
-		element2 := lines[i+1]
-		if signals.IsRightOrder(element1, element2) {
-			log.Println()
-			log.Println()
-			index += pair
+	packets := []packet{}
+	for i := 0; i < len(lines); i++ {
+		if lines[i] == "" {
+			continue
+		}
+		newPacket := packet{text: lines[i]}
+		packets = append(packets, newPacket)
+
+	}
+	dividerPackets := []packet{{text: "[[2]]"}, {text: "[[6]]"}}
+	packets = append(packets, dividerPackets...)
+	sort.Sort(byText(packets))
+	result := 1
+	for i, pack := range packets {
+		log.Println(pack)
+		if pack.text == "[[2]]" || pack.text == "[[6]]" {
+			result *= (i + 1)
 		}
 	}
-	log.Println(index)
+	log.Println(result)
 
 }
