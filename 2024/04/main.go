a69268f6af1714c50e8909951a329ed843a13e66
Aliaksei Karneyeu <korneevayu@gmail.com>
2024-12-17 15:38:55 +0100

    make day 4 work

--- 2024/04/main.go
+++ 2024/04/main.go
@@ -5,11 +5,12 @@ import (
 	"log"
 	"os"
 	"regexp"
+	"sort"
 	"strings"
 )
 
 type field struct {
-	rendered string
+	rendered [][]string
 	field    string
 	board    [][]string
 }
@@ -61,12 +62,25 @@ func findInField(field string) int {
 
 func (f *field) read(input string) {
 	f.field = strings.TrimRight(input, "\n")
+	for _, line := range strings.Split(f.field, "\n") {
+		if len(line) == 0 {
+			continue
+		}
+		l := []string{}
+		f.board = append(f.board, strings.Split(line, ""))
+		for _ = range line {
+			l = append(l, ".")
+		}
+		f.rendered = append(f.rendered, l)
+	}
 }
 
 func (f *field) render() {
-	for _, line := range f.board {
+	for _, line := range f.rendered {
 		fmt.Println(strings.Join(line, ""))
 	}
+	fmt.Println()
+	fmt.Println()
 }
 
 func (f *field) find() int {
@@ -90,66 +104,104 @@ func (f *field) find() int {
 	return count
 }
 
-func findXMAS(lines []string, Xs [][]int) int {
-	count := 0
-	newLines := []string{}
-	re := regexp.MustCompile(`.`)
-	for _, line := range lines {
-		newLines = append(newLines, re.ReplaceAll(`.`, `.`))
+func (f *field) hasXmax(x, y, dx, dy int, regex string) bool {
+
+	line := ""
+	if x+(len(regex)-1)*dx < 0 || x+(len(regex)-1)*dx >= len(f.board[0]) || y+(len(regex)-1)*dy < 0 || y+(len(regex)-1)*dy >= len(f.board) {
+		return false
 	}
+	founded := [][2]int{}
+	for i := 0; i < len(regex); i++ {
+		line += f.board[y+i*dy][x+i*dx]
+		founded = append(founded, [2]int{y + i*dy, x + i*dx})
+	}
+	if line == regex {
+		for i := 0; i < len(regex); i++ {
+			f.rendered[y+i*dy][x+i*dx] = f.board[y+i*dy][x+i*dx]
+		}
+	}
+	return line == regex
+}
+
+func (f *field) findXMAS(lines []string, Xs [][2]int) int {
+	count := 0
 	for _, X := range Xs {
-		y, x := X[0], X[1]
-		canLeft := x-4 >= 0
-		canRight := x+4 < len(lines[y])
-		canUp := y-4 >= 0
-		canDown := y+4 < len(lines)
-		if canRight && lines[y][x:x+4] == "XMAS" {
-			count++
+		for dy := -1; dy <= 1; dy++ {
+			for dx := -1; dx <= 1; dx++ {
+				if dx == 0 && dy == 0 {
+					continue
+				}
+				// log.Printf("X: %d %d", X[0], X[1])
+				if f.hasXmax(X[1], X[0], dx, dy, `XMAS`) {
+					count++
+				}
+			}
 		}
-		if canLeft && lines[y][x-4:x] == "SAMX" {
-			count++
+	}
+	return count
+}
+
+func (f *field) findMAS(lines []string, As [][2]int) int {
+	count := 0
+	for _, A := range As {
+		// log.Printf("X: %d %d", X[0], X[1])
+		if A[0] == 0 || A[0] == len(lines)-1 || A[1] == 0 || A[1] == len(lines[0])-1 {
+			continue
 		}
-		if canDown && (string(lines[y][x]) == "X" && string(lines[y+1][x]) == "M" && string(lines[y+2][x]) == "A" && string(lines[y+3][x]) == "S") {
-			count++
-		}
-		if canUp && (string(lines[y][x]) == "X" && string(lines[y-1][x]) == "M" && string(lines[y-2][x]) == "A" && string(lines[y-3][x]) == "S") {
-			count++
-		}
-		if canUp && canRight && (string(lines[y][x]) == "X" && string(lines[y-1][x+1]) == "M" && string(lines[y-2][x+2]) == "A" && string(lines[y-3][x+3]) == "S") {
-			count++
-		}
-		if canDown && canRight && (string(lines[y][x]) == "X" && string(lines[y+1][x+1]) == "M" && string(lines[y+2][x+2]) == "A" && string(lines[y+3][x+3]) == "S") {
-			count++
-		}
-		if canUp && canLeft && (string(lines[y][x]) == "X" && string(lines[y-1][x-1]) == "M" && string(lines[y-2][x-2]) == "A" && string(lines[y-3][x-3]) == "S") {
-			count++
-		}
-		if canDown && canLeft && (string(lines[y][x]) == "X" && string(lines[y+1][x-1]) == "M" && string(lines[y+2][x-2]) == "A" && string(lines[y+3][x-3]) == "S") {
+		line1 := []string{string(lines[A[0]-1][A[1]-1]), string(lines[A[0]][A[1]]), string(lines[A[0]+1][A[1]+1])}
+		line2 := []string{string(lines[A[0]+1][A[1]-1]), string(lines[A[0]][A[1]]), string(lines[A[0]-1][A[1]+1])}
+		sort.Strings(line1)
+		sort.Strings(line2)
+		if strings.Join(line1, "") == "AMS" && strings.Join(line2, "") == "AMS" {
+			f.rendered[A[0]][A[1]] = f.board[A[0]][A[1]]
+			f.rendered[A[0]-1][A[1]-1] = f.board[A[0]-1][A[1]-1]
+			f.rendered[A[0]+1][A[1]+1] = f.board[A[0]+1][A[1]+1]
+			f.rendered[A[0]+1][A[1]-1] = f.board[A[0]+1][A[1]-1]
+			f.rendered[A[0]-1][A[1]+1] = f.board[A[0]-1][A[1]+1]
+			// f.render()
+
 			count++
 		}
 	}
 	return count
 }
 
-func (f *field) find2() int {
+func (f *field) task1() int {
 	count := 0
 	log.Printf("Field:\n%s", f.field)
 	lines := strings.Split(f.field, "\n")
 	// find Xs
-	Xs := [][]int{}
+	Xs := [][2]int{}
 	for y := 0; y < len(lines); y++ {
 		for x := 0; x < len(lines[y]); x++ {
 			if lines[y][x] == 'X' {
-				Xs = append(Xs, []int{y, x})
+				Xs = append(Xs, [2]int{y, x})
 			}
 		}
 	}
-	count = findXMAS(lines, Xs)
+	count = f.findXMAS(lines, Xs)
+	return count
+}
+
+func (f *field) task2() int {
+	count := 0
+	log.Printf("Field:\n%s", f.field)
+	lines := strings.Split(f.field, "\n")
+	// find Xs
+	As := [][2]int{}
+	for y := 0; y < len(lines); y++ {
+		for x := 0; x < len(lines[y]); x++ {
+			if lines[y][x] == 'A' {
+				As = append(As, [2]int{y, x})
+			}
+		}
+	}
+	count = f.findMAS(lines, As)
 	return count
 }
 
 func main() {
-	file, err := os.ReadFile("test.txt")
+	file, err := os.ReadFile("2024/04/input.txt")
 
 	if err != nil {
 		log.Fatalf("failed to open")
@@ -158,6 +210,6 @@ func main() {
 	inputs := string(file)
 	f := field{}
 	f.read(inputs)
-	count := f.find2()
+	count := f.task2()
 	log.Println(count)
 }
